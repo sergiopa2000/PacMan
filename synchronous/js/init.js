8fc0d1f945b597022001fabd11ed3ebbe1c76626
Sergio Parejo <sergioparejo671@gmail.com>
2022-10-13 22:49:22 +0200

    graphic improvement

--- synchronous/js/init.js
+++ synchronous/js/init.js
@@ -3,7 +3,7 @@ board.startRefresh()
 
 
 let ghostsPositions = [{x: 4, y: 6}, {x: 5, y: 6}]
-let ghosts = new game.Ghost(9, ghostsPositions, 1)
+let ghosts = new game.Ghost(3, ghostsPositions, 1)
 ghosts.setUpGhosts()
 
 let player = new game.Player(1, ghosts)
